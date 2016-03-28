3ece220425cdd7e1d2ba78d684414edcd80a5b6c
Wendy Wang <wangwenusa11@gmail.com>
2016-03-28 00:11:00 -0700

    Trying to do ggplot

--- rscript.r
+++ rscript.r
@@ -1,2 +1,13 @@
-cleaned_data <- read.delim("~/cleaned_data.csv")
-View(cleaned_data)
+titanic <- read.delim("~/Documents/STAT306/TitanicAnalysis/cleaned_data.txt")
+View(titanic)
+##correlation
+correlation = cor(titanic)
+print(correlation)
+##plotting
+install.packages("ggplot2", dependencies = TRUE)
+library(ggplot2)
+ggplot(titanic, aes(x=titanic$Age, y=titanic$Survived)) 
+
+plotBinResp(titanic$Age, factor(titanic$Survived))
+plot(titanic$Fare, factor(titanic$Survived), main = "FARE")
+survival_age <- plot(titanic$Age, titanic$Survived)
